e49b366580fc42337876ca6ea8481108fae9e096
MiSkEd39 <leogonzalezy39@gmail.com>
2026-09-09 16:52:37 -0600

    relación entre colonia y problemáticas

--- app/Models/Colonia.php
+++ app/Models/Colonia.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Proyectos\Proyecto;
+use App\Models\listas\Problematicas;
 
 class Colonia extends Model
 {
@@ -32,6 +33,16 @@ class Colonia extends Model
             'proyecto_colonia', //Tabla a usar para la relación
             'colonia_id',       //FK del modelo actual
             'proyecto_id'       //FK del otro modelo
+        );
+    }
+
+    public function problematicas()
+    {
+        return $this->belongsToMany(
+            Problematicas::class,   //Modelo a relacionar
+            'problem_colonia',      //Tabla a usar para la relación
+            'colonia_id',          //FK del modelo actual
+            'problematica_id'       //FK del otro modelo
         );
     }
 }
--- app/Models/listas/Problematicas.php
+++ app/Models/listas/Problematicas.php
@@ -4,6 +4,7 @@ namespace App\Models\listas;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Proyectos\Proyecto;
+use App\Models\Colonia;
 
 class Problematicas extends Model
 {
@@ -22,6 +23,16 @@ class Problematicas extends Model
             'problematica_proyecto',//Tabla a usar para la relación
             'problematica_id',      //FK del modelo actual
             'proyecto_id'           //FK del otro modelo
+        );
+    }
+
+    public function colonias()
+    {
+        return $this->belongsToMany(
+            Colonia::class,         //Modelo a relacionar
+            'problem_colonia',      //Tabla a usar para la relación
+            'problematica_id',      //FK del modelo actual
+            'colonia_id'           //FK del otro modelo
         );
     }
 }
